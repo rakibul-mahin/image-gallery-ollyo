c9bcaa6e720d5ca3a5d4676fd8db62c086b4741e
rakibul-mahin <mahin.z.hasan137@gmail.com>
2023-11-06 17:34:53 +0600

    Resolved Image Rendering issue for Vercel

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,56 +1,56 @@
 const images = [
   {
-    url: "../../public/images/image-1.webp",
+    url: "../../public/static/images/image-1.webp",
     id: "1",
     checked: false,
   },
   {
-    url: "../../public/images/image-2.webp",
+    url: "../../public/static/images/image-2.webp",
     id: "2",
     checked: false,
   },
   {
-    url: "../../public/images/image-3.webp",
+    url: "../../public/static/images/image-3.webp",
     id: "3",
     checked: false,
   },
   {
-    url: "../../public/images/image-4.webp",
+    url: "../../public/static/images/image-4.webp",
     id: "4",
     checked: false,
   },
   {
-    url: "../../public/images/image-5.webp",
+    url: "../../public/static/images/image-5.webp",
     id: "5",
     checked: false,
   },
   {
-    url: "../../public/images/image-6.webp",
+    url: "../../public/static/images/image-6.webp",
     id: "6",
     checked: false,
   },
   {
-    url: "../../public/images/image-7.webp",
+    url: "../../public/static/images/image-7.webp",
     id: "7",
     checked: false,
   },
   {
-    url: "../../public/images/image-8.webp",
+    url: "../../public/static/images/image-8.webp",
     id: "8",
     checked: false,
   },
   {
-    url: "../../public/images/image-9.webp",
+    url: "../../public/static/images/image-9.webp",
     id: "9",
     checked: false,
   },
   {
-    url: "../../public/images/image-10.jpeg",
+    url: "../../public/static/images/image-10.jpeg",
     id: "10",
     checked: false,
   },
   {
-    url: "../../public/images/image-11.jpeg",
+    url: "../../public/static/images/image-11.jpeg",
     id: "11",
     checked: false,
   },
