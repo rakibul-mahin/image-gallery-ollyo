68cde7122b677e7f9024d1ec5172eb0d56e72a5c
rakibul-mahin <mahin.z.hasan137@gmail.com>
2023-11-06 17:44:33 +0600

    Resolving Image issue

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,56 +1,56 @@
 const images = [
   {
-    url: "../../public/image-1.webp",
+    url: "/images/image-1.webp",
     id: "1",
     checked: false,
   },
   {
-    url: "../../public/image-2.webp",
+    url: "/images/image-2.webp",
     id: "2",
     checked: false,
   },
   {
-    url: "../../public/image-3.webp",
+    url: "/images/image-3.webp",
     id: "3",
     checked: false,
   },
   {
-    url: "../../public/image-4.webp",
+    url: "/images/image-4.webp",
     id: "4",
     checked: false,
   },
   {
-    url: "../../public/image-5.webp",
+    url: "/images/image-5.webp",
     id: "5",
     checked: false,
   },
   {
-    url: "../../public/image-6.webp",
+    url: "/images/image-6.webp",
     id: "6",
     checked: false,
   },
   {
-    url: "../../public/image-7.webp",
+    url: "/images/image-7.webp",
     id: "7",
     checked: false,
   },
   {
-    url: "../../public/image-8.webp",
+    url: "/images/image-8.webp",
     id: "8",
     checked: false,
   },
   {
-    url: "../../public/image-9.webp",
+    url: "/images/image-9.webp",
     id: "9",
     checked: false,
   },
   {
-    url: "../../public/image-10.jpeg",
+    url: "/images/image-10.jpeg",
     id: "10",
     checked: false,
   },
   {
-    url: "../../public/image-11.jpeg",
+    url: "/images/image-11.jpeg",
     id: "11",
     checked: false,
   },
